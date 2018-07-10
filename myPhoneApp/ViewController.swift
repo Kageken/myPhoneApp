9b57ea21ac3859927d0619436da4fea4edb42f08
陰山賢太 <kageken@kageyamakentanoMacBook-puro.local>
2018-07-10 13:50:54 +0900

    Modified ViewController.swift

--- myPhoneApp/ViewController.swift
+++ myPhoneApp/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor{
+    class func lightBlue() -> UIColor{
+        return UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    }
+    
+    class func lightRed() -> UIColor{
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    let dateManager = DateManager()
+    let datePerWeek: Int = 7
+    let cellMargin: CGFloat = 2.0
+    var selectedDate = Date()
+    var today: Date!
+    let weekArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
     @IBOutlet weak var headarPrevButton: UIButton!
     @IBOutlet weak var headerNextButton: UIButton!
@@ -18,11 +35,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        calendarCollectionView.delegate = self
+        calendarCollectionView.dataSource = self
+        calendarCollectionView.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 7
+        } else {
+            return dateManager.daysAcquisition()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CalendarCell
+        return cell
     }
 
     @IBAction func tappedHeaderPrevButton(_ sender: UIButton) {
